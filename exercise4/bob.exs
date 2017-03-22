bc50fd2e30f3db379a31f4529569f26debd509b6
AllBecomesGood <paschnell@dundee.ac.uk>
2017-03-22 19:39:34 +0000

    Removed Comments & unnecessary ==true statements

--- exercise4/bob.exs
+++ exercise4/bob.exs
@@ -3,38 +3,16 @@ defmodule Bob do
     
 	cond do
 		
-		String.split(input, " ", trim: true) |> length == 0 -> "Fine. Be that way!"
+		String.split(input, " ", trim: true) 
+		|> length == 0 -> "Fine. Be that way!"
 		
-		questionTest(input) == true -> "Sure."
-		
-		shoutTest(input) == true -> "Whoa, chill out!"
+		questionTest(input) -> "Sure."
+		# Easier way found in other solution.
+		#String.ends_with?(input, "?") -> "Sure."
+
+		shoutTest(input) -> "Whoa, chill out!"
 		
 		true -> "Whatever."
-		
-		#true -> raise "Your implementation goes here"
-        #String.length(input)==0 -> "Fine. Be that way!"
-		#"_" <> "?" == input ->
-		#input == "_" <> "?" ->
-		#63 in input
-		#Enum.member?(input, "?")==true ->
-		#List.last(input)=="?" -> 
-		#String.contains?(input, "?") ->
-		# "Sure."
-		#"?" == List.last(input) -> "Sure."
-		#"?" = hd(Enum.reverse(list));
-		# need to split that string somehow. it counts as one element listName = ["asdf?"] so head and tail are == "asdf?"
-		#pattern = :binary.compile_pattern([" ", "?"])
-		#String.split(input, pattern) == "?" -> "Sure."
-		# the above removes the ? from the end
-		#& <> "?" == input -> "Sure."
-		#String.graphemes(hd(input)) == "?" -> 
-		 #"Sure."
-		#[head | tail] = Enum.reverse(String.graphemes(input))
-		#converting to uppercase messes with later functions?, would be good to do it temporarily only
-		#input == String.upcase(input) ->
-		# "Whoa, chill out!"
-		 
-		
     end
   end
   
